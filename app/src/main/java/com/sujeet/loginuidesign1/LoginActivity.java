5b58534b3bab0f932fea85f58755a4b72d9cea2b
gahoisathi@gmail.com <Gahoi@123#$>
2019-12-16 22:18:21 +0530

    first comit

---
+++ app/src/main/java/com/sujeet/loginuidesign1/LoginActivity.java
@@ -0,0 +1,35 @@
+package com.sujeet.loginuidesign1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_login);
+
+
+
+
+        ((TextView)findViewById(R.id.signIN)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            }
+        });
+
+
+
+    }
+}
